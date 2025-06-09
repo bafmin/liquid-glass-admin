d571461f38e2cd2cba35075154340302b3e52b73
Aegis AI <aegis@bafmin.com>
2025-06-09 17:23:08 -0500

    feat(ui): implement GlassSidebar menu with NavLink items

--- src/ui/GlassSidebar/index.tsx
+++ src/ui/GlassSidebar/index.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { GlassPanel } from '../GlassPanel';
+import { navItems } from '../../common/navItems';
 import styles from './GlassSidebar.module.css';
 
 export const GlassSidebar: React.FC = () => (
   <GlassPanel className={styles.sidebar}>
-    {/* TODO: Add sidebar items */}
+    <ul className={styles.menu}>
+      {navItems.map(item => (
+        <li key={item.path}>
+          <NavLink
+            to={item.path}
+            className={({ isActive }) =>
+              `${styles.menuItem} ${isActive ? styles.active : ''}`
+            }
+          >
+            {item.icon && <span className={styles.icon}>{item.icon}</span>}
+            <span className={styles.label}>{item.label}</span>
+          </NavLink>
+        </li>
+      ))}
+    </ul>
   </GlassPanel>
 );
